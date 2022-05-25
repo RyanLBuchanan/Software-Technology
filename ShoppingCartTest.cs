bdc27d68465b6e44108c03c07736ca5733db940e
Ryan Buchanan <buchanan.ryan22@gmail.com>
2022-05-25 14:30:26 -0600

    feat: Modified Deliverable 4a doc

--- ShoppingCartTest.cs
+++ ShoppingCartTest.cs
@@ -57,75 +57,75 @@
 
 
 
-///* FROM CKK.TESTSFORSTUDENTS  from Module 4 Deliverable 3 */
+/* FROM CKK.TESTSFORSTUDENTS  from Module 4 Deliverable 3 */
 
-////[Fact]
-////public void AddStoreItem_ShouldAddProductToNextSpot_FromEmpty()
-////{
-////    try
-////    {
-////        //Assemble
-////        Store store = new();
-////        var expected = new Product();
-////        store.AddStoreItem(expected);
-////        //Act
-////        var actual = store.GetStoreItem(1);
-////        //Assert
-////        Assert.Equal(expected, actual);
-////    }
-////    catch
-////    {
-////        throw new XunitException("The product was not populated correctly.");
-////    }
-////}
+//[Fact]
+//public void AddStoreItem_ShouldAddProductToNextSpot_FromEmpty()
+//{
+//    try
+//    {
+//        //Assemble
+//        Store store = new();
+//        var expected = new Product();
+//        store.AddStoreItem(expected);
+//        //Act
+//        var actual = store.GetStoreItem(1);
+//        //Assert
+//        Assert.Equal(expected, actual);
+//    }
+//    catch
+//    {
+//        throw new XunitException("The product was not populated correctly.");
+//    }
+//}
 
 
-////[Fact]
-////public void RemoveStoreItem_ShouldRemoveSelectedItem()
-////{
-////    try
-////    {
-////        //Assemble
-////        Store store = new();
-////        var product1 = new Product();
-////        var product2 = new Product();
-////        var product3 = new Product();
-////        store.AddStoreItem(product1);
-////        store.AddStoreItem(product2);
-////        store.AddStoreItem(product3);
+//[Fact]
+//public void RemoveStoreItem_ShouldRemoveSelectedItem()
+//{
+//    try
+//    {
+//        //Assemble
+//        Store store = new();
+//        var product1 = new Product();
+//        var product2 = new Product();
+//        var product3 = new Product();
+//        store.AddStoreItem(product1);
+//        store.AddStoreItem(product2);
+//        store.AddStoreItem(product3);
 
-////        //Act
-////        store.RemoveStoreItem(1);
+//        //Act
+//        store.RemoveStoreItem(1);
 
-////        //Assert
-////        Assert.Null(store.GetStoreItem(1));
-////    }
-////    catch
-////    {
-////        throw new XunitException("The item was not removed correctly");
-////    }
-////}
+//        //Assert
+//        Assert.Null(store.GetStoreItem(1));
+//    }
+//    catch
+//    {
+//        throw new XunitException("The item was not removed correctly");
+//    }
+//}
 
-////[Fact]
-////public void GetTotal_ShouldReturnTheCorrectTotal()
-////{
-////    try
-////    {
-////        //Assemble
-////        var price = 4.0m;
-////        var quantity = 2;
-////        var expected = 8m;
-////        var testProduct = new Product();
-////        testProduct.SetPrice(price);
+//[Fact]
+//public void GetTotal_ShouldReturnTheCorrectTotal()
+//{
+//    try
+//    {
+//        //Assemble
+//        var price = 4.0m;
+//        var quantity = 2;
+//        var expected = 8m;
+//        var testProduct = new Product();
+//        testProduct.SetPrice(price);
 
-////        var cartItem = new ShoppingCartItem(testProduct, quantity);
-////        //Act
-////        var actual = cartItem.GetTotal();
-////        //Assert
-////        Assert.Equal(expected, actual);
-////    }
-////    catch
-////    {
-////        throw new XunitException("The Price that was given was incorrect.");
-////    }
-////}
+//        var cartItem = new ShoppingCartItem(testProduct, quantity);
+//        //Act
+//        var actual = cartItem.GetTotal();
+//        //Assert
+//        Assert.Equal(expected, actual);
+//    }
+//    catch
+//    {
+//        throw new XunitException("The Price that was given was incorrect.");
+//    }
+//}
