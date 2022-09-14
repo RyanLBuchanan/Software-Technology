15fb53d3b4b38b5e1a99ce6700cce1369a577e77
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2022-09-14 10:04:03 -0600

    feat: Saved current PROG1500 Module 5 SQL script

---
+++ Courses/PROG1500 - Databases and SQL/Modules/Module 5 - Date and Conversion Functions/Script-home.sql
@@ -0,0 +1,20 @@
+--SELECT first_name, last_name, phone
+--	FROM student
+--WHERE registration_date = '23-FEB-07'
+--ORDER BY last_name, first_name;
+
+--SELECT course_no, section_id, start_date_time
+--	FROM section
+--WHERE location = 'L214'
+--ORDER BY start_date_time;
+
+--SELECT course_no, section_id, start_date_time, instructor_id
+--	FROM section 
+--WHERE start_date_time BETWEEN '01-APR-07' AND '01-MAY-07' 
+--ORDER BY course_no, section_no;
+
+--SELECT student_id, section_id, final_grade
+--	FROM enrollment 
+--WHERE enroll_date BETWEEN '01-JAN-07' AND '01-FEB-07' 
+--	AND final_grade IS NOT NULL;
+
