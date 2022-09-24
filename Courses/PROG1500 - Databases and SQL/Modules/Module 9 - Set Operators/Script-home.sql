68c3077ad4eae141fdcec441f8367c92969f8c9b
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2022-09-24 13:20:50 -0600

    feat: Added solutions to PROG1500 Module 9

--- Courses/PROG1500 - Databases and SQL/Modules/Module 9 - Set Operators/Script-home.sql
+++ Courses/PROG1500 - Databases and SQL/Modules/Module 9 - Set Operators/Script-home.sql
@@ -1,12 +1,25 @@
 --1.	Provide a list of students that have enrolled in a course that allowed them an opportunity to drop a lowest score.
-
-
-
+SELECT last_name, first_name
+	FROM student
+WHERE student_id IN
+	(SELECT e.student_id
+		FROM enrollment e, section s
+	WHERE e.section_id = s.section_id
+		AND e.section_id IN
+			(SELECT section_id
+				FROM grade_type_weight
+			WHERE drop_lowest = 'Y'))
+ORDER BY last_name;
 
 
 --2.	List the names and phone numbers of instructors who teach students that live in same zipcode as the instructor.
-
-
+--SELECT i.last_name, i.first_name, i.phone
+--	FROM instructor i, student s, section t, enrollment e 
+--WHERE i.zip = s.zip
+--	AND i.instructor_id = t.instructor_id
+--	AND s.student_id = e.student_id
+--	AND e.section_id = t.section_id
+--ORDER BY i.last_name;
 
 
 
