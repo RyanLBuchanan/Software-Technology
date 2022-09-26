00a44f43167dec0cb055695eae6c4db47a94a4b3
Ryan Buchanan <buchanan.ryan22@gmail.com>
2022-09-26 13:22:10 -0600

    feat: Completed PROG1500 Module 12

---
+++ Courses/PROG1500 - Databases and SQL/Modules/Module 12 - Create Alter and Drop Tables/Script-quiz.sql
@@ -0,0 +1,29 @@
+--A parent table referenced by a child table may not be dropped.
+--TRUE
+
+--The data of a temporary table is visible to all sessions.
+--TRUE
+
+--By default, the foreign key restricts deletes of any parent row that has a corresponding child row(s).
+--TRUE
+
+--The ADD and MODIFY keywords can be used interchangeably in an ALTER TABLE statement.
+--TRUE
+
+--It is possible to create one table from three different tables in a single CREATE TABLE statement.
+--TRUE
+
+--The NUMBER data type is usually the best choice for a primary key.
+--True
+
+--Primary key values should always be subject to frequent change.
+--FALSE
+
+--A table can be created with or without data.
+--TRUE
+
+--You can add to and drop columns from a table using the ALTER TABLE command.
+--TRUE
+
+
+
